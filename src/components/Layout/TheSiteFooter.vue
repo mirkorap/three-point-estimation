e8bf930384a9d9af5ce6a67b4acc9813c19c8b44
Mirko Rapisarda <mirko@paneedesign.com>
2020-04-12 16:36:54 +0200

    feat: add "header" and "footer" components

---
+++ src/components/Layout/TheSiteFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-footer
+      app
+      class="font-weight-medium">
+    <v-col
+        class="text-center"
+        cols="12">
+      {{ new Date().getFullYear() }} — <strong>Three-point Estimation</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'TheSiteFooter',
+  };
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Layout/TheSiteHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app-bar
+      app
+      color="primary"
+      dark>
+    <div class="d-flex align-center">
+      <v-img
+          class="shrink mr-2"
+          contain
+          src="@/assets/logo.svg"
+          transition="scale-transition"
+          width="180">
+      </v-img>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'TheSiteHeader',
+  };
+</script>
+
+<style scoped>
+
+</style>
