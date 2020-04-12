f3217dccd0ee188178299e5366106db30569b872
Mirko Rapisarda <mirko@paneedesign.com>
2020-04-12 16:24:06 +0200

    chore: install ad run eslint-config-airbnb

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,67 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint"
-  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-alert': 'off',
+    'no-param-reassign': 'off',
+    'max-len': ['error', 120],
+    'vue/html-indent': [
+      'error', 2, {
+        attribute: 2,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: [],
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      2, {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: true,
+        },
+      },
+    ],
+    'vue/html-self-closing': [
+      'error', {
+        html: {
+          void: 'never',
+          normal: 'never',
+          component: 'never',
+        },
+      },
+    ],
+    'vue/html-closing-bracket-newline': [
+      'error', {
+        singleline: 'never',
+        multiline: 'never',
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      rules: {
+        indent: 'off',
+        'vue/script-indent': [
+          'error', 2, {
+            baseIndent: 1,
+            switchCase: 1,
+          },
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 };
